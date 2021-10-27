5792cc9c31c8b94c6846f44158763e05156d740c
Maxime66410 <max66410@hotmail.fr>
2021-10-27 14:23:51 +0200

    Optimization Light | FIX IA

---
+++ Assets/Scripts/GameManager/CameraTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+
+    [SerializeField] private GameObject CameraSystem;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CameraSystem.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CameraSystem.SetActive(false);
+        }
+    }
+}
